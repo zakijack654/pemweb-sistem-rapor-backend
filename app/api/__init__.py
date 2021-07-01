3b12cf2ecf5a7c1674c6742c6677918d90e2a8ab
SyauqiMA <syauqiabrar@gmail.com>
2021-07-01 15:57:41 +0700

    registeres new blueprints

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -15,6 +15,10 @@ def create_app():
     
     # Register blueprints
     from .routes.siswa import siswa
+    from .routes.jurusan import jurusan
+    from .routes.mapel import mapel
     app.register_blueprint(siswa, url_prefix='/api')
+    app.register_blueprint(jurusan, url_prefix='/api')
+    app.register_blueprint(mapel, url_prefix='/api')
 
     return app
